5ccf81ba26d5a597233ba91235d4bf963a5313da
Markus Graf <info@markusgraf.ch>
2018-02-19 12:48:32 +0100

    remove unnecessary require

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative 'boot'
 
 require 'rails/all'
-require 'pana_validation_questionary_structure'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
