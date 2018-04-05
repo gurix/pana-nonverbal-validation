f7395a810213fc7ef6c88d038f3a0156899ea253
Markus Graf <info@markusgraf.ch>
2018-04-05 09:49:24 +0200

    adjusted calculation of duration

--- data_analysis/prepare_data.R
+++ data_analysis/prepare_data.R
@@ -1,7 +1,11 @@
 # Read data
-raw_data <- read.csv('data/201803291613.csv')
+raw_data <- read.csv('data/201804050932.csv')
 # Start with real data from id 36 on
 raw_data <- subset(raw_data, id > 36)
 
 # Questionary is completed when pan is filled out
 completed <- subset(raw_data, pan20 > 0)
+
+# Calculate the speed in minutes
+completed$duration <- as.numeric(difftime(as.POSIXct(strptime(completed$updated_at, "%Y-%m-%d %H:%M")), as.POSIXct(strptime(completed$started_at, "%Y-%m-%d %H:%M"))))
+
